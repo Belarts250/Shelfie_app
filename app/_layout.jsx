53c03a19bed825819d5841d8586b0de40f33da93
Igihozo Belise <igihozobelise6@gmail.com>
2025-12-18 19:28:56 +0200

    added themedview

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -6,7 +6,7 @@ import { Colors } from '../constants/Colors.js'
 const RootLayout = () => { 
 
     const colorScheme = useColorScheme()
-    const theme = Colors[colorScheme] ?? Colors.light
+    const theme = Colors[colorScheme] 
 
     return (
       <View style ={{flex: 1}} >
@@ -14,8 +14,8 @@ const RootLayout = () => {
         {/* <Stack /> */}
         <Stack screenOptions={{
             headerStyle : {
-                backgroundColor: theme.navBackground},
-                headerTintColor: theme.title
+                backgroundColor: theme.background},
+                headerTintColor: theme.title,              
         }}>
             <Stack.Screen name="index" options = {{title: 'Home'}} />
             <Stack.Screen name="about" options = {{title: 'About Us', headerShown: true}} />
---
+++ components/ThemedView.jsx
@@ -0,0 +1,20 @@
+import { View , useColorScheme} from 'react-native'
+import { Colors } from '../constants/Colors'
+
+const ThemedView = ({style}) => { 
+    const colorScheme = useColorScheme()
+    const theme = Colors[colorScheme] ??  Colors.light
+
+
+    return (
+      <View style = {[{
+        backgroundColor: theme.background
+      }, style]}>
+            {/* <Text>This is a card component</Text> */}
+      </View>
+    )
+  }
+
+
+export default ThemedView
+const styles = StyleSheet.create({})
